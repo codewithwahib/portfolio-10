93019d72d6ea42e18edfa45bdf104389dce1e27e
codewithwahib <wahibjaffer043@gmail.com>
2024-11-11 13:42:53 +0500

    Describe what you changed here

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -6,17 +6,17 @@ const Hero = () => {
     <div
       id="hero"
       className="min-h-screen bg-no-repeat bg-[url(/profile.jpg)] bg-cover"
-      style={{ backgroundSize: '35%', backgroundPosition: 'left 100px top 150px' }}
+      style={{ backgroundSize: '35%', backgroundPosition: 'left 100px top 200px' }}
     >
       <Navbar />
       <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
         <div className="hidden lg:block"></div>
         <div className="text-[70px] sm:text-[100px] font-bold leading-tight flex justify-center items-start mt-[200px]">
-          <div>
+          <b><b><b><div>
             <p data-aos="zoom-in-left" className="text-orange-500">I&apos;m</p>
             <p data-aos="zoom-in-left" className="text-orange-500">Hassan</p>
             <p data-aos="zoom-in-left" className="text-orange-500">Jaffer</p>
-          </div>
+          </div></b></b></b>
         </div>
       </div>
     </div>
